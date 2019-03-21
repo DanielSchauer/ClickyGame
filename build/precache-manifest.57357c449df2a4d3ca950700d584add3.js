138637a05f69e260acc12b789bc67dd8754f4533
Daniel Schauer <dschauer17@gmail.com>
2019-03-21 18:03:33 -0400

    final

---
+++ build/precache-manifest.57357c449df2a4d3ca950700d584add3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "9b5896e7b4889714c6d6",
+    "url": "/ClickyGame/static/js/main.72f62b44.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/ClickyGame/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "9b5896e7b4889714c6d6",
+    "url": "/ClickyGame/static/css/main.7d1e9548.chunk.css"
+  },
+  {
+    "revision": "dc2509d26d4e4504af2d6287cd238cca",
+    "url": "/ClickyGame/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from'./Header.js';
-import './Header.css';
-import Footer from './Footer.js';
-import './Footer.css';
-import Nav from './Nav.js'
-import './Nav.css'
-import MarioCard from  "./components/CharacterCard/Mario"
-import BowserCard from  "./components/CharacterCard/Bowser"
-import LuigiCard from  "./components/CharacterCard/Luigi"
-import PeachCard from  "./components/CharacterCard/Peach"
-import DaisyCard from  "./components/CharacterCard/Daisy"
-import WarioCard from  "./components/CharacterCard/Wario"
-import WaluigiCard from  "./components/CharacterCard/Waluigi"
-import BowserJrCard from  "./components/CharacterCard/BowserJr"
-import DKCard from  "./components/CharacterCard/DK"
-import DiddyKongCard from  "./components/CharacterCard/DiddyKong"
-import RosalinaCard from  "./components/CharacterCard/Rosalina"
-import YoshiCard from  "./components/CharacterCard/Yoshi"
+import Header from'./components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
+import Nav from './components/Nav/Nav.js'
 //Game starts with a score of 0
 //Score increases if image is clicked
 //Score resets if same image is clicked twice
 //Images resuffle after a image is clicked
 //Game restarts if Score is 0
-
-
-
-
-
-
 
 class App extends Component {
   render() {
@@ -38,18 +17,7 @@ class App extends Component {
         <Nav />
         <Header />
 
-        <MarioCard />
-        <LuigiCard />
-        <PeachCard />
-        <DaisyCard />
-        <RosalinaCard />
-        <YoshiCard />
-        <WarioCard />
-        <WaluigiCard />
-        <BowserCard />
-        <BowserJrCard />
-        <DKCard />
-        <DiddyKongCard />
+        
 
         <Footer />
       </div>
